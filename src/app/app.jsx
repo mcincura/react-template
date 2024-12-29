e07934d6be8442e3c86dcdca22858f35cfc55d45
mcincura <mar.cincura@gmail.com>
2024-12-29 13:34:26 +0100

    React Template

---
+++ src/app/app.jsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+
+const App = () => {
+    return(
+        <div className='main-content'>
+            <h1>TEMPLATE</h1>
+        </div>
+    )
+};
+
+export default App
